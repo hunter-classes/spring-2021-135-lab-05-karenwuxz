7bb7f025f8a29ecc981a3c20d27603078689d0a5
Karen Wu <karenwu@Karens-MacBook-Pro.local>
2021-03-03 20:16:49 -0500

    added main tests

--- main.cpp
+++ main.cpp
@@ -5,27 +5,39 @@
 
 int main(){
   //Task A
-  // std::cout << "Testing if 100 is divisable by 25: " << isDivisibleBy(100,25) << std::endl;
-  // std::cout << "Testing if -90 is divisable by 5: " << isDivisibleBy(-90,5) << std::endl;
-  // std::cout << "Testing if 0 is divisable by 0: " << isDivisibleBy(0,0) << std::endl;
-  // std::cout << "Testing if 10 is divisable by 0: " << isDivisibleBy(10,0) << std::endl;
+  std::cout << "Testing if 100 is divisable by 25: " << isDivisibleBy(100,25) << std::endl;
+  std::cout << "Testing if -90 is divisable by 5: " << isDivisibleBy(-90,5) << std::endl;
+  std::cout << "Testing if 0 is divisable by 0: " << isDivisibleBy(0,0) << std::endl;
+  std::cout << "Testing if 10 is divisable by 0: " << isDivisibleBy(10,0) << std::endl;
 
-  //Task B
-  // std::cout << "Is 10 prime: " << isPrime(10)<< std::endl;
-  // std::cout << "Is 2 prime: " << isPrime(2) << std::endl;
-  // std::cout << "Is 0 prime: " << isPrime(0) << std::endl;
-  // std::cout << "Is -14 prime: " << isPrime(-14) << std::endl;
+  // Task B
+  std::cout << "Is 10 prime: " << isPrime(10)<< std::endl;
+  std::cout << "Is 2 prime: " << isPrime(2) << std::endl;
+  std::cout << "Is 0 prime: " << isPrime(0) << std::endl;
+  std::cout << "Is -14 prime: " << isPrime(-14) << std::endl;
 
-  //Task C
-  // std::cout << "Next prime number after 8: " << nextPrime(8) << std::endl;
-  // std::cout << "Answer is prime: " << isPrime(nextPrime(8)) << std::endl;
-  // std::cout << "Next prime number after 1113106: " << nextPrime(1113106) << std::endl;
-  // std::cout << "Answer is prime: " << isPrime(nextPrime(1113106)) << std::endl;
+  // Task C
+  std::cout << "Next prime number after 8: " << nextPrime(8) << std::endl;
+  std::cout << "Next prime number after 1113106: " << nextPrime(1113106) << std::endl;
 
   //Task D
   std::cout << "amount of prime number 1-12: " << countPrimes(1,12) << std::endl;
+  std::cout << "amount of prime number 24-50: " << countPrimes(24,50) << std::endl;
+  std::cout << "amount of prime number 100-200: " << countPrimes(100,200) << std::endl;
 
   //Task E
-  // std::cout << isTwinPrime(11) << std::endl;
+  std::cout << "Is 11 a twin Prime: " << isTwinPrime(11) << std::endl;
+  std::cout << "Is 1 a twin Prime: " << isTwinPrime(1) << std::endl;
+  std::cout << "Is 17207 a twin Prime: " << isTwinPrime(17207) << std::endl;
+
+  //Task F
+  std::cout << "The next twin prime after 2 is " << nextTwinPrime(2) << std::endl;
+  std::cout << "The next twin prime after 25 is " << nextTwinPrime(25) << std::endl;
+  std::cout << "The next twin prime after 0 is " << nextTwinPrime(0) << std::endl;
+
+  //Task G
+  std::cout << "The largest twin prime from 1-10 " <<largestTwinPrime(1,10) <<std::endl;
+  std::cout << "The largest twin prime from 24-50 " <<largestTwinPrime(24,50) <<std::endl;
+  std::cout << "The largest twin prime from 100-200 " <<largestTwinPrime(100,200) <<std::endl;
   return 0;
 }
